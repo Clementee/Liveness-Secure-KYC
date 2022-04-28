382c4180beff396f74eddd1363c2168d59874a6d
Clément Sanh <61148897+Clementee@users.noreply.github.com>
2022-04-28 19:42:23 +0200

    Clean the code to only keep what is required

--- gui/MainWindow.py
+++ gui/MainWindow.py
@@ -1,31 +1,3 @@
-#
-# python_grabber
-#
-# Authors:
-#  Andrea Schiavinato <andrea.schiavinato84@gmail.com>
-#
-# Copyright (C) 2019 Andrea Schiavinato
-#
-# Permission is hereby granted, free of charge, to any person obtaining
-# a copy of this software and associated documentation files (the
-# "Software"), to deal in the Software without restriction, including
-# without limitation the rights to use, copy, modify, merge, publish,
-# distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so, subject to
-# the following conditions:
-#
-# The above copyright notice and this permission notice shall be
-# included in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-#
-
 import queue
 from tkinter import messagebox
 
@@ -39,7 +11,24 @@ from gui.image_process import *
 
 
 class MainWindow:
+
     def __init__(self, master):
+        """
+        @param self:
+        @param master:
+        """
+        self.change_camera_btn = None
+        self.lbl_status1 = None
+        self.save_btn = None
+        self.master = None
+        self.grab_btn = None
+        self.canvas = None
+        self.plot = None
+        self.image_controls_area2 = None
+        self.image_controls_area = None
+        self.image_area = None
+        self.status_area = None
+        self.video_area = None
         self.create_gui(master)
         self.grabber = PyGrabber(self.on_image_received)
         self.queue = queue.Queue()
@@ -48,28 +37,28 @@ class MainWindow:
         self.select_device()
 
     def create_gui(self, master):
+        """
+        @param self: The main window on which we apply the creation
+        @param master:
+        """
         self.master = master
         master.title("Python Photo App")
-        self.create_menu(master)
 
         master.columnconfigure(0, weight=1, uniform="group1")
         master.columnconfigure(1, weight=1, uniform="group1")
         master.rowconfigure(0, weight=1)
 
         self.video_area = Frame(master, bg='black')
-        self.video_area.grid(row=0, column=0, sticky=W+E+N+S, padx=5, pady=5)
+        self.video_area.grid(row=0, column=0, sticky=W + E + N + S, padx=5, pady=5)
 
         self.status_area = Frame(master)
-        self.status_area.grid(row=1, column=0, sticky=W+E+N+S, padx=5, pady=5)
+        self.status_area.grid(row=1, column=0, sticky=W + E + N + S, padx=5, pady=5)
 
         self.image_area = Frame(master)
-        self.image_area.grid(row=0, column=1, sticky=W+E+N+S, padx=5, pady=5)
+        self.image_area.grid(row=0, column=1, sticky=W + E + N + S, padx=5, pady=5)
 
         self.image_controls_area = Frame(master)
         self.image_controls_area.grid(row=1, column=1, padx=5, pady=0)
-
-        self.image_controls_area2 = Frame(master)
-        self.image_controls_area2.grid(row=2, column=1, padx=5, pady=0)
 
         # Grabbed image
         fig = Figure(figsize=(5, 4), dpi=100)
@@ -88,37 +77,18 @@ class MainWindow:
         self.grab_btn = Button(self.image_controls_area, text="Grab", command=self.grab_frame)
         self.grab_btn.pack(padx=5, pady=20, side=LEFT)
 
-        self.save_btn = Button(self.image_controls_area2, text="Save", command=self.save_image)
-        self.save_btn.pack(padx=5, pady=2, side=LEFT)
+        self.save_btn = Button(self.image_controls_area, text="Save", command=self.save_image)
+        self.save_btn.pack(padx=5, pady=2, side=RIGHT)
+
+        self.change_camera_btn = Button(self.image_controls_area, text="Change Camera", command=self.change_camera)
+        self.change_camera_btn.pack(padx=5, pady=2, side=RIGHT)
 
         self.video_area.bind("<Configure>", self.on_resize)
-
-    def create_menu(self, master):
-        menubar = Menu(master)
-        self.master.config(menu=menubar)
-
-        camera_menu = Menu(menubar)
-        camera_menu.add_command(label="Open...", command=self.change_camera)
-        camera_menu.add_command(label="Set properties...", command=self.camera_properties)
-        camera_menu.add_command(label="Set format...", command=self.set_format)
-        camera_menu.add_command(label="Start preview", command=self.start_preview)
-        camera_menu.add_command(label="Stop", command=self.stop)
-        menubar.add_cascade(label="Camera", menu=camera_menu)
-
-        image_menu = Menu(menubar)
-        image_menu.add_command(label="Grab image", command=self.grab_frame)
-        image_menu.add_command(label="Save...", command=self.save_image)
-        menubar.add_cascade(label="Image", menu=image_menu)
-
-        recording_menu = Menu(menubar)
-        recording_menu.add_command(label="Start recording...", command=self.start_stop_recording)
-        recording_menu.add_command(label="Stop recording", command=self.stop)
-        menubar.add_cascade(label="Video Recording", menu=recording_menu)
 
     def display_image(self):
         while self.queue.qsize():
             try:
-                self.image = self.queue.get(0)
+                self.image = self.queue.get()
                 self.original_image = self.image
                 self.plot.imshow(np.flip(self.image, axis=2))
                 self.canvas.draw()
@@ -157,43 +127,11 @@ class MainWindow:
     def on_resize(self, event):
         self.grabber.update_window(self.video_area.winfo_width(), self.video_area.winfo_height())
 
-    def init_device(self):
-        self.grabber.start()
-
     def grab_frame(self):
         self.grabber.grab_frame()
 
-    def start_stop_recording(self):
-        audio_devices = self.grabber.get_audio_devices()
-        video_compressors = self.grabber.get_video_compressors()
-        audio_compressors = self.grabber.get_audio_compressors()
-        asf_profiles = self.grabber.get_asf_profiles()
-        input_dialog = ConfigureRecording(self.master, audio_devices, video_compressors, audio_compressors, asf_profiles)
-        self.master.wait_window(input_dialog.top)
-        if input_dialog.result:
-            try:
-                self.grabber.start_recording(input_dialog.get_audio_device_index(),
-                                             input_dialog.get_video_compressor_index(),
-                                             input_dialog.get_audio_compressor_index(),
-                                             input_dialog.get_filename(),
-                                             self.video_area.winfo_id())
-                self.grabber.update_window(self.video_area.winfo_width(), self.video_area.winfo_height())
-                self.display_status(self.grabber.get_status())
-            except:
-                messagebox.showinfo("Error", "An error occurred during the recoding. Select a different comnination of compressors and try again.")
-                self.display_status(self.grabber.get_status())
-
     def on_image_received(self, image):
         self.queue.put(image)
-
-    def start_preview(self):
-        self.grabber.start_preview(self.video_area.winfo_id())
-        self.display_status(self.grabber.get_status())
-        self.on_resize(None)
-
-    def stop(self):
-        self.grabber.stop()
-        self.display_status(self.grabber.get_status())
 
     def save_image(self):
         filename = filedialog.asksaveasfilename(
@@ -202,19 +140,3 @@ class MainWindow:
             filetypes=[('PNG', ".png"), ('JPG', ".jpg")])
         if filename is not None:
             save_image(filename, self.image)
-
-    def image_filter(self, process_function):
-        def inner():
-            if self.original_image is None:
-                return
-            self.image = process_function(self.original_image)
-            self.plot.imshow(np.flip(self.image, axis=2))
-            self.canvas.draw()
-        return inner
-
-    def restore_original_image(self):
-        if self.original_image is None:
-            return
-        self.image = self.original_image
-        self.plot.imshow(np.flip(self.image, axis=2))
-        self.canvas.draw()
